0bb4edbc1b00337d68f072f686b00715ce0b4295
Saulo Mendes <saulomendes_br@hotmail.com>
2024-11-18 20:52:27 -0300

    Changing the port number of the server - test

--- src/test/java/br/com/smms/config/TestConfigs.java
+++ src/test/java/br/com/smms/config/TestConfigs.java
@@ -2,7 +2,7 @@ package br.com.smms.config;
 
 public class TestConfigs {
 
-	public static final int SERVER_PORT = 80;
+	public static final int SERVER_PORT = 8888;
 	public static final String CONTENT_TYPE_JSON = "application/json";
 	
 }
